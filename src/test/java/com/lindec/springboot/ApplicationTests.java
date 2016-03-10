fd358a81dc8524f1a8eeeec6d286f6db769fee15
lindechang <504828318@qq.com>
2016-03-10 16:42:10 +0800

    commit

--- src/test/java/com/lindec/springboot/ApplicationTests.java
+++ src/test/java/com/lindec/springboot/ApplicationTests.java
@@ -1,11 +1,11 @@
 package com.lindec.springboot;
 
-import com.lindec.springboot.repository.Person;
 import com.lindec.springboot.repository.PersonRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.core.env.Environment;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -15,12 +15,18 @@ public class ApplicationTests {
 	@Autowired
 	private PersonRepository personRepository;
 
+	@Autowired
+	private Environment env;
+
 	@Test
 	public void contextLoads() {
-		Person person = new Person();
-		person.setAge(156);
-		person.setName("lindec");
-		personRepository.save(person);
+//		Person person = new Person();
+//		person.setAge(156);
+//		person.setName("lindec");
+//		personRepository.save(person);
+
+		System.out.println("------------------------------------");
+		System.out.println("--------" + env.getProperty("app.tcpPort"));
 
 	}
 
